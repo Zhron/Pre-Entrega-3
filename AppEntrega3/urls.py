af89b735a753ee1e57c383823559bd4ff2fd6959
Santiago Zboron <santizboron@gmail.com>
2024-07-30 16:33:13 -0300

    Intentando hacer usuarios

---
+++ AppEntrega3/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for Clases_Coder project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from AppEntrega3 import views
+
+urlpatterns = [
+    path('', views.inicio, name="Inicio"),
+    path('about/', views.about, name="About")
+]
+
+# Cursos
+urlpatterns += [
+    path('curso-list/', views.CursoListView.as_view(), name="CursoList"),
+    path('curso-detail/<int:pk>/', views.CursoDetailView.as_view(), name="CursoDetail"),
+    path('curso-create/', views.CursoCreateView.as_view(), name="CursoCreate"),
+    path('curso-update/<int:pk>/', views.CursoUpdateView.as_view(), name="CursoUpdate"),
+    path('curso-delete/<int:pk>/', views.CursoDeleteView.as_view(), name="CursoDelete"),
+]
+
+# Estudiantes
+urlpatterns += [
+    path('estudiante-list/', views.EstudianteListView.as_view(), name="EstudianteList"),
+    path('estudiante-detail/<int:pk>/', views.EstudianteDetailView.as_view(), name="EstudianteDetail"),
+    path('estudiante-create/', views.EstudianteCreateView.as_view(), name="EstudianteCreate"),
+    path('estudiante-update/<int:pk>/', views.EstudianteUpdateView.as_view(), name="EstudianteUpdate"),
+    path('estudiante-delete/<int:pk>/', views.EstudianteDeleteView.as_view(), name="EstudianteDelete"),
+]
+
+# # Profesores
+urlpatterns += [
+    path('profesor-list/', views.ProfesorListView.as_view(), name="ProfesorList"),
+    path('profesor-detail/<int:pk>/', views.ProfesorDetailView.as_view(), name="ProfesorDetail"),
+    path('profesor-create/', views.ProfesorCreateView.as_view(), name="ProfesorCreate"),
+    path('profesor-update/<int:pk>/', views.ProfesorUpdateView.as_view(), name="ProfesorUpdate"),
+    path('profesor-delete/<int:pk>/', views.ProfesorDeleteView.as_view(), name="ProfesorDelete"),
+]
--- AppEntrega3/views.py
+++ AppEntrega3/views.py
@@ -1,3 +1,137 @@
 from django.shortcuts import render
+from .models import Curso
+from AppEntrega3.forms import CursoFormulario, BuscaCursoForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Curso, Estudiante, Profesor, Entregable
+from django.urls import reverse_lazy
+from users.models import Imagen
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Dejamos la vista INICIO basada en funciones y visible para todos
+def inicio(request):
+    # imagen = Imagen.objects.filter(user=request.user.id)[0]
+    # print(imagen)
+    return render(request, "AppEntrega3/index.html")# , {"url": imagen})
+
+
+# Dejamos una vista basada en funciones que requiere login para mostrar el uso de @login_required
+@login_required
+def about(request):
+    return render(request, "AppEntrega3/about.html")
+
+
+# VISTAS BASADAS EN CLASES - CURSOS
+class CursoListView(LoginRequiredMixin, ListView):
+    model = Curso
+    template_name = "AppEntrega3/curso_list.html"
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class CursoDetailView(LoginRequiredMixin, DetailView):
+    model = Curso
+    template_name = "AppEntrega3/curso_detail.html"
+
+    # login_url = '/users/login/'
+
+    # def get_login_url(self):
+    #     return self.login_url
+
+
+class CursoCreateView(LoginRequiredMixin, CreateView):
+    """
+    Esta clase envía por defecto un formulario al archivo html. Envía los campos indicados en la lista "fields" y podemos hacer uso de dicho formulario bajo la key "form".
+    """
+
+    model = Curso
+    template_name = "AppEntrega3/curso_create.html"
+    fields = ["nombre", "camada"]
+
+    # En success_url indicamos la vista que queremos visitar una vez que se genera un curso con éxito. Lo podemos hacer de 2 formas:
+    
+    # Indicando la URL
+    # success_url = "/curso-list/"
+    # Con el reverse_lazy indicamos el nombre de la vista
+    success_url = reverse_lazy("CursoList")
+
+
+class CursoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Curso
+    success_url = reverse_lazy("CursoList")
+    fields = ["nombre", "camada"]
+    template_name = "AppEntrega3/curso_update.html"
+
+
+class CursoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Curso
+    success_url = reverse_lazy("CursoList")
+    template_name = 'AppEntrega3/curso_confirm_delete.html'
+
+
+
+class EstudianteListView(LoginRequiredMixin, ListView):
+    model = Estudiante
+    template_name = "AppEntrega3/estudiante_list.html"
+
+
+class EstudianteDetailView(LoginRequiredMixin, DetailView):
+    model = Estudiante
+    template_name = "AppEntrega3/estudiante_detail.html"
+
+
+class EstudianteCreateView(LoginRequiredMixin, CreateView):
+
+    model = Estudiante
+    template_name = "AppEntrega3/estudiante_create.html"
+    fields = ["nombre", "apellido", "email"]
+    success_url = reverse_lazy("EstudianteList")
+
+
+class EstudianteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Estudiante
+    success_url = reverse_lazy("EstudianteList")
+    fields = ["nombre", "apellido", "email"]
+    template_name = "AppEntrega3/estudiante_update.html"
+
+
+class EstudianteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Estudiante
+    success_url = reverse_lazy("EstudianteList")
+    template_name = 'AppEntrega3/estudiante_confirm_delete.html'
+
+
+
+class ProfesorListView(LoginRequiredMixin, ListView):
+    model = Profesor
+    template_name = "AppEntrega3/profesor_list.html"
+
+
+class ProfesorDetailView(LoginRequiredMixin, DetailView):
+    model = Profesor
+    template_name = "AppEntrega3/profesor_detail.html"
+
+
+class ProfesorCreateView(LoginRequiredMixin, CreateView):
+    model = Profesor
+    template_name = "AppEntrega3/profesor_create.html"
+    fields = ["nombre", "apellido", "email"]
+    success_url = reverse_lazy("ProfesorList")
+
+
+class ProfesorUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profesor
+    success_url = reverse_lazy("ProfesorList")
+    fields = ["nombre", "apellido", "email"]
+    template_name = "AppEntrega3/profesor_update.html"
+
+
+class ProfesorDeleteView(LoginRequiredMixin, DeleteView):
+    model = Profesor
+    success_url = reverse_lazy("ProfesorList")
+    template_name = 'AppEntrega3/profesor_confirm_delete.html'
